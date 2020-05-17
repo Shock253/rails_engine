d0151760c98eb56e13d53bc6617c89427d2063ff
Ethan Hocking <eshocking253@gmail.com>
2020-05-17 11:36:50 -0600

    Remove WorldSeed.from_csv and just use load_csv

--- db/seeders/world_seed.rb
+++ db/seeders/world_seed.rb
@@ -1,24 +1,15 @@
 require 'csv'
-require '../../app/models/customer'
 
 class WorldSeed
-  def self.from_csv
-    raise "Don't run this in production!" if Rails.env.production?
-
-    Customer.delete_all
-    load_csv('customers.csv') do |row|
-      binding.pry
-      t = Customer.new
-      t.name
-    end
-  end
-
   def self.load_csv(filename, &block)
-    csv_text = File.read(Rails.root.join('lib', 'seeds', filename))
+    raise "Don't run this in production!" if Rails.env.production?
+    csv_text = File.read(Rails.root.join('db', 'seeds', filename))
     csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
     csv.each do |row|
       model = block.call(row)
-      model.save
+      if !model.save
+        raise "There's something wrong with the model data..."
+      end
     end
   end
 end
