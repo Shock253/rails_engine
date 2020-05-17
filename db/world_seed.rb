09cc19f20997831515f9d482e5630621d2ee2666
Ethan Hocking <eshocking253@gmail.com>
2020-05-17 13:15:35 -0600

    Fix some rubocop errors

--- db/world_seed.rb
+++ db/world_seed.rb
@@ -3,22 +3,24 @@ require 'csv'
 class WorldSeed
   def self.load_csv(filename, model_class)
     raise "Don't run this in production!" if Rails.env.production?
+
     model_class.delete_all
     models = []
-    CSV.foreach(Rails.root.join('db', 'seeds', filename), headers: true) do |row|
+    path = Rails.root.join('db', 'seeds', filename)
+    CSV.foreach(path, headers: true) do |row|
       models << model_class.new(row.to_h)
     end
     model_class.import models, recursive: true
   end
 
-  def self.verify (model, filename)
-
-  end
+  # def self.verify (model, filename)
+  #
+  # end
 
   private
 
   def self.open_csv(filename)
     csv_text = File.read(Rails.root.join('db', 'seeds', filename))
-    CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+    CSV.parse(csv_text, headers: true, encoding: 'ISO-8859-1')
   end
 end
