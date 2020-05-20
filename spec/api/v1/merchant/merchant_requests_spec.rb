9dc2f600351ba58d318c937759f868c5835a53ff
Ethan Hocking <eshocking253@gmail.com>
2020-05-20 14:55:14 -0600

    Add spec for merchant api show action

--- spec/api/v1/merchant/merchant_requests_spec.rb
+++ spec/api/v1/merchant/merchant_requests_spec.rb
@@ -11,7 +11,23 @@ RSpec.describe 'Merchant API request: ' do
     merchants = JSON.parse(response.body)
 
     expect(merchants.keys).to contain_exactly('data')
+    expect(merchants['data'].length).to eq(3)
     expect(merchants['data'][0].keys).to contain_exactly('id', 'type', 'attributes')
     expect(merchants['data'][0]['attributes'].keys).to contain_exactly('name')
+  end
+
+  it "Show" do
+    id = create(:merchant).id
+
+    get "/api/v1/merchants/#{id}"
+
+    expect(response).to be_successful
+
+    merchant = JSON.parse(response.body)
+
+    expect(merchant.keys).to contain_exactly('data')
+    expect(merchant['data'].keys).to contain_exactly('id', 'type', 'attributes')
+    expect(merchant['data']['id']).to eq("#{id}")
+    expect(merchant['data']['attributes'].keys).to contain_exactly('name')
   end
 end
